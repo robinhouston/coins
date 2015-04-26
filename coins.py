23705d1d9409f26ea02fd9682a4721e8ec8a3301
Robin Houston <robin.houston@gmail.com>
2015-04-27 00:08:23 +0100

    Add a doc comment to the partitions function

--- coins.py
+++ coins.py
@@ -4,6 +4,8 @@ COINS = (1, 2, 5, 10, 20, 50, 100, 200)
 TARGET = 3975
 
 def partitions(n, k):
+    """All the partitions of n into k positive integers.
+    """
     if k == 1:
         yield (n,)
         return
